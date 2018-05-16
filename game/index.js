16ea4a9d18e8e6c7e0b5942519a4457dc015a3dd
Huu Nguyen <huu.nguyen@gmail.com>
2018-05-16 12:12:57 -0400

    board

--- game/index.js
+++ game/index.js
@@ -1,6 +1,18 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable';
+let board = Map();
 
-export default function reducer(state, action) {
+board
+  .setIn([1, 1], '')
+  .setIn([1, 2], '')
+  .setIn([1, 3], '')
+  .setIn([2, 1], '')
+  .setIn([2, 2], '')
+  .setIn([2, 3], '')
+  .setIn([3, 1], '')
+  .setIn([3, 2], '')
+  .setIn([3, 3], '');
+
+export default function reducer(state = { turn: 'X', board: board }, action) {
   // TODO
-  return state
+  return state;
 }
